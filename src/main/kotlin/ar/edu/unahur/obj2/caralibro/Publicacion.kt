1df96c6fe49e8f5524a4cb738525c040236c0a6b
Matias Gonzalez <matias.gonzalez@live.com.ar>
2021-04-17 19:05:18 -0300

    commit tipo de publicacion video. Sd, 720p, 1080p

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
@@ -7,10 +7,44 @@ abstract class Publicacion {
 }
 
 class Foto(val alto: Int, val ancho: Int) : Publicacion() {
-  val factorDeCompresion = 0.7
+  var factorDeCompresion = 0.7
   override fun espacioQueOcupa() = ceil(alto * ancho * factorDeCompresion).toInt()
+
+  fun cambiarFactorDeCompresion( nuevoFactorDeCompresion: Double)
+  {
+    factorDeCompresion = nuevoFactorDeCompresion
+  }
 }
 
 class Texto(val contenido: String) : Publicacion() {
   override fun espacioQueOcupa() = contenido.length
 }
+
+class Video(val duracionDelVideo : Int, val calidad : Calidad) : Publicacion() {
+
+  override fun espacioQueOcupa(): Int {
+    return ceil(calidad.calcularCalidad(duracionDelVideo).toDouble()).toInt()
+  }
+}
+
+
+abstract class Calidad
+{
+abstract fun calcularCalidad(segundos : Int) : Int
+}
+
+class CalidadSd() : Calidad()
+{
+  override fun calcularCalidad(segundos : Int) = segundos
+}
+
+open class Calidad720() : Calidad()
+{
+  override fun calcularCalidad(segundos : Int) = segundos * 3
+}
+
+class Calidad1080() : Calidad720()
+{
+  override fun calcularCalidad(segundos : Int) = super.calcularCalidad(segundos) * 2
+}
+
--- src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
@@ -7,6 +7,9 @@ class UsuarioTest : DescribeSpec({
   describe("Caralibro") {
     val saludoCumpleanios = Texto("Felicidades Pepito, que los cumplas muy feliz")
     val fotoEnCuzco = Foto(768, 1024)
+    val videoSD = Video(10,CalidadSd())
+    val video720 = Video(10,Calidad720())
+    val video1080 = Video(10,Calidad1080())
 
     describe("Una publicación") {
       describe("de tipo foto") {
@@ -20,6 +23,26 @@ class UsuarioTest : DescribeSpec({
           saludoCumpleanios.espacioQueOcupa().shouldBe(45)
         }
       }
+
+      describe("de tipo Video Sd") {
+        it("ocupa tantos bytes como su tiempo de duracion") {
+          videoSD.espacioQueOcupa().shouldBe(10)
+        }
+      }
+
+      describe("de tipo Video 720p") {
+        it("ocupa tantos bytes como su tiempo de duracion por 3") {
+          video720.espacioQueOcupa().shouldBe(30)
+        }
+      }
+
+      describe("de tipo Video 1080p") {
+        it("ocupa tantos bytes como su tiempo de duracion el doble de uno 720p") {
+          video1080.espacioQueOcupa().shouldBe(60)
+        }
+      }
+
+
     }
 
     describe("Un usuario") {
