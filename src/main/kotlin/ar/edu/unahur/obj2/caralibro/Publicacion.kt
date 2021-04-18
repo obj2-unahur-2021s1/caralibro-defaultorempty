7a271eff5208f68fa23c0b16f0242cc89cb9a5a3
Matias Gonzalez <matias.gonzalez@live.com.ar>
2021-04-18 10:39:05 -0300

    requerimiento 2

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
@@ -2,49 +2,93 @@ package ar.edu.unahur.obj2.caralibro
 
 import kotlin.math.ceil
 
-abstract class Publicacion {
+abstract class Publicacion(var tipoPermiso : Permiso) {
+
+  val listaDeLikes = mutableListOf<Usuario>()
+
   abstract fun espacioQueOcupa(): Int
-}
+  abstract fun cambiarTipoDePermiso(permiso: Permiso): Unit
 
-class Foto(val alto: Int, val ancho: Int) : Publicacion() {
-  var factorDeCompresion = 0.7
-  override fun espacioQueOcupa() = ceil(alto * ancho * factorDeCompresion).toInt()
+  open fun agregarLike(usuario: Usuario) {
+    if (listaDeLikes.count()!= 0 && listaDeLikes.contains(usuario)) {
+      error("Publicacion ya likeada")
+    } else {
+      listaDeLikes.add(usuario)
+    }
+  }
 
-  fun cambiarFactorDeCompresion( nuevoFactorDeCompresion: Double)
-  {
-    factorDeCompresion = nuevoFactorDeCompresion
+  open fun cantidadDeLikes() : Int {
+    return listaDeLikes.size
   }
 }
 
-class Texto(val contenido: String) : Publicacion() {
-  override fun espacioQueOcupa() = contenido.length
-}
+  class Foto(val alto: Int, val ancho: Int, tipoPermiso: Permiso) : Publicacion(tipoPermiso) {
+    var factorDeCompresion = 0.7
+    override fun espacioQueOcupa() = ceil(alto * ancho * factorDeCompresion).toInt()
 
-class Video(val duracionDelVideo : Int, val calidad : Calidad) : Publicacion() {
+    fun cambiarFactorDeCompresion(nuevoFactorDeCompresion: Double) {
+      factorDeCompresion = nuevoFactorDeCompresion
+    }
 
-  override fun espacioQueOcupa(): Int {
-    return ceil(calidad.calcularCalidad(duracionDelVideo).toDouble()).toInt()
+    override fun cambiarTipoDePermiso(permiso: Permiso) {
+      tipoPermiso = permiso
+    }
+
   }
-}
+
+  class Texto(val contenido: String, tipoPermiso: Permiso) : Publicacion(tipoPermiso) {
+    override fun espacioQueOcupa() = contenido.length
+
+    override fun cambiarTipoDePermiso(permiso: Permiso) {
+      tipoPermiso = permiso
+    }
+  }
+
+  class Video(val duracionDelVideo: Int, val calidad: Calidad, tipoPermiso: Permiso) : Publicacion(tipoPermiso) {
+
+    override fun espacioQueOcupa(): Int {
+      return ceil(calidad.calcularCalidad(duracionDelVideo).toDouble()).toInt()
+    }
+
+    override fun cambiarTipoDePermiso(permiso: Permiso) {
+      tipoPermiso = permiso
+    }
+  }
 
 
-abstract class Calidad
-{
-abstract fun calcularCalidad(segundos : Int) : Int
-}
+  abstract class Calidad {
+    abstract fun calcularCalidad(segundos: Int): Int
+  }
 
-class CalidadSd() : Calidad()
-{
-  override fun calcularCalidad(segundos : Int) = segundos
-}
+  class CalidadSd() : Calidad() {
+    override fun calcularCalidad(segundos: Int) = segundos
+  }
 
-open class Calidad720() : Calidad()
-{
-  override fun calcularCalidad(segundos : Int) = segundos * 3
-}
+  open class Calidad720() : Calidad() {
+    override fun calcularCalidad(segundos: Int) = segundos * 3
+  }
 
-class Calidad1080() : Calidad720()
-{
-  override fun calcularCalidad(segundos : Int) = super.calcularCalidad(segundos) * 2
-}
+  class Calidad1080() : Calidad720() {
+    override fun calcularCalidad(segundos: Int) = super.calcularCalidad(segundos) * 2
+  }
 
+  abstract class Permiso() {
+    abstract fun puedeVerPublicacion(amigo: Usuario): Boolean
+  }
+
+
+  class PermisoPublico() : Permiso() {
+    override fun puedeVerPublicacion(amigo: Usuario) = true
+  }
+
+  class PermisoSoloAmigos(val amigos: List<Usuario>) : Permiso() {
+    override fun puedeVerPublicacion(amigo: Usuario) = amigos.contains(amigo)
+  }
+
+  class PermisoPrivadoConListaDePermitidos(val listaDePermitidos: List<Usuario>) : Permiso() {
+    override fun puedeVerPublicacion(amigo: Usuario) = listaDePermitidos.contains(amigo)
+  }
+
+  class PermisoPublicoConListaDeExcluidos(val listaDeExcluidos: List<Usuario>) : Permiso() {
+    override fun puedeVerPublicacion(amigo: Usuario) = !listaDeExcluidos.contains(amigo)
+  }
--- src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
@@ -2,14 +2,17 @@ package ar.edu.unahur.obj2.caralibro
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.assertions.shouldFail
+import io.kotest.assertions.throwables.shouldThrowAny
 
 class UsuarioTest : DescribeSpec({
   describe("Caralibro") {
-    val saludoCumpleanios = Texto("Felicidades Pepito, que los cumplas muy feliz")
-    val fotoEnCuzco = Foto(768, 1024)
-    val videoSD = Video(10,CalidadSd())
-    val video720 = Video(10,Calidad720())
-    val video1080 = Video(10,Calidad1080())
+    val saludoCumpleanios =
+      Texto("Felicidades Pepito, que los cumplas muy feliz", tipoPermiso = PermisoPublico())
+    val fotoEnCuzco = Foto(768, 1024, tipoPermiso = PermisoPublico())
+    val videoSD = Video(10, CalidadSd(), tipoPermiso = PermisoPublico())
+    val video720 = Video(10, Calidad720(), tipoPermiso = PermisoPublico())
+    val video1080 = Video(10, Calidad1080(), tipoPermiso = PermisoPublico())
 
     describe("Una publicación") {
       describe("de tipo foto") {
@@ -50,8 +53,52 @@ class UsuarioTest : DescribeSpec({
         val juana = Usuario()
         juana.agregarPublicacion(fotoEnCuzco)
         juana.agregarPublicacion(saludoCumpleanios)
-        juana.espacioDePublicaciones().shouldBe(550548)
+        juana.agregarPublicacion(videoSD)
+        juana.agregarPublicacion(video720)
+        juana.agregarPublicacion(video1080)
+
+        //juana.espacioDePublicaciones().shouldBe(550548) sin videos
+        juana.espacioDePublicaciones().shouldBe( 550648) // con videos 550548 + 10 + 30 + 60
       }
     }
+
+    describe("Un usuario con publicaciones y likes") {
+      it("puede calcular el espacio que ocupan sus publicaciones") {
+        val juana = Usuario()
+        juana.agregarPublicacion(fotoEnCuzco)
+        juana.agregarPublicacion(saludoCumpleanios)
+        juana.agregarPublicacion(videoSD)
+        juana.agregarPublicacion(video720)
+        juana.agregarPublicacion(video1080)
+
+        //juana.espacioDePublicaciones().shouldBe(550548) sin videos
+        juana.espacioDePublicaciones().shouldBe( 550648) // con videos 550548 + 10 + 30 + 60
+      }
+    }
+
+    describe("Una publicaciones y likes") {
+      it("Texto y me gusta de un usuario") {
+        val bienvenida = Texto("Bienvenidos",tipoPermiso = PermisoPublico() )
+        val pedro = Usuario()
+
+        bienvenida.agregarLike(pedro)
+
+        bienvenida.cantidadDeLikes().shouldBe(1)
+      }
+    }
+
+    describe("Una publicaciones y 2 likes del mismo usuario") {
+      it("Texto y me gusta de un usuario") {
+        val bienvenida = Texto("Bienvenidos",tipoPermiso = PermisoPublico() )
+        val pedro = Usuario()
+
+        val exception = shouldThrowAny {
+          bienvenida.agregarLike(pedro)
+          bienvenida.agregarLike(pedro)
+        }
+
+      }
+    }
+
   }
 })
